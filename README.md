506c622fbc88610bd3ed89ff24a6f7072ead2094
Yoann Archambault <ya@sherpa-solutions.com>
2025-07-17 11:39:48 -0400

    Docker-compose setup + JDBC/Postgres skeleton.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# entrevue-mariana
+# Air Quality Test
+
+To start the docker compose stack (postgres), execute this in this project directory:
+
+    docker-compose up -d
+
+When you no longer need the postgres server:
+
+    docker-compose down
--- src/main/java/com/sherpa/entrevue/Application.java
+++ src/main/java/com/sherpa/entrevue/Application.java
@@ -1,13 +1,22 @@
 package com.sherpa.entrevue;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @Configuration
 public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
 
     public static void main(String[] args) {
         SpringApplicationBuilder app = new SpringApplicationBuilder(Application.class);
@@ -15,4 +24,18 @@ public class Application {
         app.run(args);
     }
 
+
+    @Bean
+    public CommandLineRunner verifyDatabaseConnection(JdbcTemplate jdbcTemplate) {
+        return args -> {
+            try {
+                Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+                logger.info("Database is reachable: " + result);
+            } catch (Exception e) {
+                logger.info("Failed to connect to the database:", e);
+
+                System.exit(1); // non-zero exit code on failure
+            }
+        };
+    }
 }
